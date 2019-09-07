4c579729d1165b704d036b711b9e068e4a5bab50
Sergey Ukhlivanov <ukhlivanov@gmail.com>
2019-09-07 11:31:14 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
 import React from 'react';
-import logo from './logo.svg';
+import Clock from './Components/Clock'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+  }
+
+
+  componentDidUpdate(prevProps, prevState){
+    console.log(this.state)
+    if(this.state.count === 0){
+      clearInterval(this.timer)
+    }
+  }
+
+  handleStart = () => {
+    this.timer = setInterval(()=>{
+      const newCount = this.state.count-1
+      this.setState({
+        count: newCount >=0 ? newCount : 0
+      })
+    }, 1000)
+    this.userInput.value=''
+     }
+
+  handleStop = () => {
+    if(this.timer){
+      clearInterval(this.timer)
+    }
+    this.userInput.value=''
+  }
+
+  handleReset = () => {
+    this.setState({
+      count: 0
+    })
+  }
+
+  handleCountdown =() =>{
+    if(this.userInput.value.match(/[0-9]/)){
+      this.setState({
+        count: this.userInput.value,
+      })
+    }else{
+      alert('Please, use only numbers')
+      this.userInput.value=''
+    }
+
+  }
+
+  render() {
+    const {count} = this.state
+    return (
+      <div>
+        <Clock time={count}/>
+        <input
+          type='text'
+          placeholder='enter time in seconds'
+          ref = {userInput => this.userInput = userInput}
+          onChange = {this.handleCountdown}
+        />
+        <br></br>
+        <input
+          type='button'
+          value='start'
+          onClick={this.handleStart}
+        />
+
+        <input
+          type='button'
+          value='stop'
+          onClick={this.handleStop}
+        />
+
+        <input
+          type='button'
+          value='reset'
+          onClick={this.handleReset}
+        />
+
+      </div>
+    )
+  }
 }
 
 export default App;
